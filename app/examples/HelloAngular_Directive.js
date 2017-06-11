b3564344245e59982657c58f18e007b3f96345fb
smile_ucas <xiaoxiaotvb@126.com>
2017-06-11 16:55:28 +0800

    restrict

--- app/examples/HelloAngular_Directive.js
+++ app/examples/HelloAngular_Directive.js
@@ -1,8 +1,8 @@
 var myModule = angular.module("MyModule", []);
 myModule.directive("hello", function() {
     return {
-        restrict: 'E',
-        template: '<div>Hi everyone!</div>',
+        restrict: 'AEMC',//A属性(默认)，E元素，C样式类，M注释
+        template: '<div>Hi everyone!</div>',  //可以使用templateUrl:'something.html'
         replace: true
     }
 });

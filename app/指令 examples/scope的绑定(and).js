1329c8523dff0afaf82947e2125a89ebe542e0bd
smile_ucas <xiaoxiaotvb@126.com>
2017-06-19 15:51:15 +0800

    scope的绑定（and）

---
+++ app/指令 examples/scope的绑定(and).js
@@ -0,0 +1,19 @@
+/**
+ * Created by xiaoxiao on 2017/6/19.
+ */
+var myModule=angular.module("MyModule",[]);
+myModule.controller('MyCtrl', ['$scope', function($scope){
+    $scope.sayHello=function (name) {
+        alert('Hello,'+name)
+    }
+}])
+myModule.directive("greeting",function () {
+    return {
+        restrict:'AE',
+        scope:{
+            greet:'&'
+        },
+        template:'<input type="text" ng-model="userName" /><br/>'+
+            '<button class="btn btn-default" ng-click="greet({name:userName})">Greeting</button><br/> '
+    }
+})

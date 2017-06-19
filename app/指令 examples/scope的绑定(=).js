5fc0bcb6ec076d87f829f4874d88ceaaa3cf445a
smile_ucas <xiaoxiaotvb@126.com>
2017-06-19 14:22:19 +0800

    scope的绑定（=）

---
+++ app/指令 examples/scope的绑定(=).js
@@ -0,0 +1,16 @@
+/**
+ * Created by xiaoxiao on 2017/6/19.
+ */
+var myModule = angular.module("MyModule", []);
+myModule.controller('MyCtrl', ['$scope', function($scope){
+    $scope.ctrlFlavor="百威";
+}])
+myModule.directive("drink", function() {
+    return {
+        restrict:'AE',
+        scope:{
+            flavor:'='
+        },
+        template:'<input type="text" ng-model="flavor"/>'
+    }
+});

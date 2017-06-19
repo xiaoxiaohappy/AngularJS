05637a622f1f8a2c6d63036176e61f36ce577fe1
smile_ucas <xiaoxiaotvb@126.com>
2017-06-19 14:14:10 +0800

    绑定scope @

--- app/指令 examples/scope的绑定(@).js
+++ app/指令 examples/scope的绑定(@).js
@@ -1,3 +1,29 @@
 /**
  * Created by xiaoxiao on 2017/6/19.
  */
+var myModule=angular.module("MyModule",[]);
+myModule.controller('MyCtrl', ['$scope', function($scope){
+    $scope.ctrlFlavor="百威";
+}])
+
+//一般的方式  link
+// myModule.directive("drink", function() {
+//     return {
+//         template:"<div>{{flavor}}</div>",
+//         link:function(scope,element,attrs){
+//         	scope.flavor=attrs.flavor;
+//         }
+//     }
+// });
+
+
+//以下是用绑定的方式  @
+myModule.directive("drink", function() {
+    return {
+        restrict:'AE',
+        scope:{
+            flavor:'@'
+        },
+        template:"<div>{{flavor}}</div>"
+    }
+});

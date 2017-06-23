cd78b21a42dd041a8593286cf00f6c079b966eed
smile_ucas <xiaoxiaotvb@126.com>
2017-06-23 16:13:09 +0800

    accordion_expander

--- app/指令 examples/自定义指令(expander_使用ng-repeat循环expamders数组).js
+++ app/指令 examples/自定义指令(expander_使用ng-repeat循环expamders数组).js
@@ -1,53 +1,61 @@
-/**
- * Created by YXX on 2017/6/22.
- */
-var manyExpanderModule = angular.module('myexpanders',[])
+var expModule=angular.module('myexpanders',[])
 
-manyExpanderModule.directive('myManyExpander',function () {
+//accordion不可自己随意命名
+expModule.directive('accordion', function() {
     return {
-        restrict:'EA',
-        replace:true,
-        tansclude:true,
-        template:'<div ng-transclude></div>',
-        controller:function () {
-            var expanders=[]
-            this.gotOpened=function (selectedExpander) {
-                angular.forEach(expanders,function (expander) {
-                    if(selectedExpander!=expander){
-                        expander.showMe=false
+        restrict : 'EA',
+        replace : true,
+        transclude : true,
+        template : '<div ng-transclude></div>',
+        controller : function() {
+            var expanders = [];
+            this.gotOpened = function(selectedExpander) {
+                angular.forEach(expanders, function(expander) {
+                    if (selectedExpander != expander) {
+                        expander.showMe = false;
                     }
-                })
+                });
             }
-            this.addExpander=function (expander) {
-                expanders.push(expander)
+            this.addExpander = function(expander) {
+                expanders.push(expander);
             }
         }
     }
-})
+});
 
-manyExpanderModule.directive('expander',function () {
-    return{
-        restrict:'EA',
-        replace:true,
-        transclude:true,
-
+expModule.directive('expander', function() {
+    return {
+        restrict : 'EA',
+        replace : true,
+        transclude : true,
+        require : '^?accordion',
+        scope : {
+            title : '=expanderTitle'
+        },
+        template : '<div>'
+        + '<div class="title" ng-click="toggle()">{{title}}</div>'
+        + '<div class="body" ng-show="showMe" ng-transclude></div>'
+        + '</div>',
+        link : function(scope, element, attrs, accordionController) {
+            scope.showMe = false;
+            accordionController.addExpander(scope);
+            scope.toggle = function toggle() {
+                scope.showMe = !scope.showMe;
+                accordionController.gotOpened(scope);
+            }
+        }
     }
-})
+});
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+expModule.controller("myCtrl",function($scope) {
+    $scope.expanders = [{
+        title : 'Click me to expand',
+        text : 'Hi there folks, I am the content that was hidden but is now shown.'
+    }, {
+        title : 'Click this',
+        text : 'I am even better text than you have seen previously'
+    }, {
+        title : 'Test',
+        text : 'test'
+    }];
+});

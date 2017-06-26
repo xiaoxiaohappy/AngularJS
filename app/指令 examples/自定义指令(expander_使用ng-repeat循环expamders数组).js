19b663fe11d64961d6909806dc4c8f2761807f5e
smile_ucas <xiaoxiaotvb@126.com>
2017-06-26 21:59:25 +0800

    .

--- app/指令 examples/自定义指令(expander_使用ng-repeat循环expamders数组).js
+++ app/指令 examples/自定义指令(expander_使用ng-repeat循环expamders数组).js
@@ -9,6 +9,8 @@ expModule.directive('accordion', function() {
         template : '<div ng-transclude></div>',
         controller : function() {
             var expanders = [];
+
+            //this指controller
             this.gotOpened = function(selectedExpander) {
                 angular.forEach(expanders, function(expander) {
                     if (selectedExpander != expander) {
@@ -28,6 +30,11 @@ expModule.directive('expander', function() {
         restrict : 'EA',
         replace : true,
         transclude : true,
+
+
+        //'^?accordion' ?后面写的是包含这个指令的那个父指令（这个是指令嵌套用的），表示向外层寻找accordion指令,直到找到为止.注意:向外层寻找,也包括了自己本身,也就是说,自己可以寻找到自己身上的其它指令
+        //'?'的作用是如果找不到也不会报错
+        //定义了require属性后,link函数的第四个参数ctrl就是require到的指令的controller方法的实例.所以ctrl就有了addChild方法了.
         require : '^?accordion',
         scope : {
             title : '=expanderTitle'
@@ -36,12 +43,13 @@ expModule.directive('expander', function() {
         + '<div class="title" ng-click="toggle()">{{title}}</div>'
         + '<div class="body" ng-show="showMe" ng-transclude></div>'
         + '</div>',
-        link : function(scope, element, attrs, accordionController) {
+        link : function(scope, element, attrs, ctrl) {
+            //ctrl参数是我之前require的controller实例
             scope.showMe = false;
-            accordionController.addExpander(scope);
+            ctrl.addExpander(scope);
             scope.toggle = function toggle() {
                 scope.showMe = !scope.showMe;
-                accordionController.gotOpened(scope);
+                ctrl.gotOpened(scope);
             }
         }
     }
